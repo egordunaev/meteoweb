a4e4118e1d503a8ba2cd02faaa25bcb7e4e24c01
Egor <egodunaev@gmail.com>
2019-11-18 04:42:45 +0300

    changed weather from forecast to current

--- server/weather_data/openweathermap/openweathermap.py
+++ server/weather_data/openweathermap/openweathermap.py
@@ -12,8 +12,14 @@ class OpenWeather:
         self.city_id = self.get_city_id(self.country, self.city)
 
     def get_city_id(self, country, city):
-        return "temp"
+        with open("server/weather_data/openweathermap/city.list.json", encoding="utf8") as cities_json:
+            cities = json.load(cities_json)
+        for item in cities:
+            if item["name"].lower() is city and item["country"] is country:
+                return item
+        return None
 
     def get_weather(self, city_id):
-        weather = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?id={city_id}&APPID={api_key}", headers={"User-Agent": "Mozilla/5.0"})
+        weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?id={city_id}&APPID={api_key}", headers={"User-Agent": "Mozilla/5.0"})
+        # weather_dict = weather.json() - use this when finished
         return weather.json()
